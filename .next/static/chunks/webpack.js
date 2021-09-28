0a0e94cf672c4f34c738871255249a5a5c937cf3
matt-shade-nbcuni <matt.shade@nbcuni.com>
2021-09-28 18:08:02 -0400

    adds footer

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "05ab37401ee86bf52eae";
+/******/ 	var hotCurrentHash = "2589a50709025c959190";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
